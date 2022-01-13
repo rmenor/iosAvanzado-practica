c68bc3e313c2e7bdcc593fa9b0a8d758b44ba502
Ramón Menor <ramonmenor@gmail.com>
2022-01-13 21:10:47 +0100

    Añado target watch-ios

---
+++ whatch WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  whatch WatchKit Extension
+//
+//  Created by Ramón Menor Molina on 13/1/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ whatch WatchKit Extension/NotificationView.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationView.swift
+//  whatch WatchKit Extension
+//
+//  Created by Ramón Menor Molina on 13/1/22.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
