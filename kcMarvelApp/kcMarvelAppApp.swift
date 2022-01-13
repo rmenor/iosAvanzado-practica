5d9ad32f53d934fb9e32605c229d93b886e3b8a2
Ramón Menor <ramonmenor@gmail.com>
2022-01-13 08:54:46 +0100

    Inicio aplicación

---
+++ kcMarvelApp/kcMarvelAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  kcMarvelAppApp.swift
+//  kcMarvelApp
+//
+//  Created by Ramón Menor Molina on 12/1/22.
+//
+
+import SwiftUI
+
+@main
+struct kcMarvelAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
